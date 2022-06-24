53561d97be3c88ee1697405c788f53f87875a210
Tomás Coêlho <tomascoelho6@gmail.com>
2022-06-24 09:13:51 -0300

    update lists index

--- lists/index.rb
+++ lists/index.rb
@@ -1,9 +1,31 @@
 require 'json'
+require 'aws-sdk-dynamodb'
+require 'singleton'
 
 module Lists
-  module Index
-    def self.handler(event:, context:)
-      {statusCode: 200, body: JSON.generate({hi: 'ya'})}
+  class Index
+    include Singleton
+
+    def self.handler(...)
+      instance.handler(...)
     end
+
+    def initialize
+      @table = Aws::DynamoDB::Table.new(ENV.fetch('LISTS_TABLE'), Aws::DynamoDB::Client.new)
+    end
+
+    def handler(event:, context:)
+      result = table.scan.items
+
+      {
+        body: JSON.fast_generate(result),
+        headers: {'Content-Type' => 'application/json'},
+        statusCode: 200,
+      }
+    end
+
+    private
+
+    attr_reader :table
   end
 end
